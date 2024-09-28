0f1861945a3fd0d9341b29a70db44c9c1758f3a5
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-28 13:40:10 +0200

    Add implicit wait to support synchronization

--- WDExamples/TS_OrangeHRMLogin.py
+++ WDExamples/TS_OrangeHRMLogin.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+driver.implicitly_wait(5)
 
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
