a2c18111a27e50f017135f63f6e8ec24b7ea5d6a
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-28 12:40:51 +0200

    Initial commit

---
+++ WDExamples/TS_LaunchGooglePage.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com/")
+
+title = driver.title
+
+print("Title is", title)
+
+driver.close()
