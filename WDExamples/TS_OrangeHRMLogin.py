190a58e9e7b0616217096d79df8321915745d7d9
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-28 13:37:09 +0200

    Write tests for OrangeHRM (not synchronized)

---
+++ WDExamples/TS_OrangeHRMLogin.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+driver.maximize_window()
+
+driver.find_element(By.NAME, "username").send_keys("Admin")
+
+driver.find_element(By.NAME, "password").send_keys("admin123")
+
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+driver.find_element(By.CLASS_NAME, "oxd-userdropdown-tab").click()
+
+driver.find_element(By.LINK_TEXT, "Logout").click()
+
+driver.close()
